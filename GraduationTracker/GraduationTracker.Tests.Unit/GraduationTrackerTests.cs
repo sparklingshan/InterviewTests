c459557e6e8eff99fe9cd8019fb869fe4516eb2c
Hao <hao.qiao7@gmail.com>
2018-07-30 21:42:21 -0400

    updates

--- GraduationTracker/GraduationTracker.Tests.Unit/GraduationTrackerTests.cs
+++ GraduationTracker/GraduationTracker.Tests.Unit/GraduationTrackerTests.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -44,45 +44,47 @@ namespace GraduationTracker.Tests.Unit
                         new Course{Id = 4, Name = "Physichal Education", Mark=80 }
                    }
                },
-            new Student
-            {
-                Id = 3,
-                Courses = new Course[]
+                new Student
                 {
-                    new Course{Id = 1, Name = "Math", Mark=50 },
-                    new Course{Id = 2, Name = "Science", Mark=50 },
-                    new Course{Id = 3, Name = "Literature", Mark=50 },
-                    new Course{Id = 4, Name = "Physichal Education", Mark=50 }
-                }
-            },
-            new Student
-            {
-                Id = 4,
-                Courses = new Course[]
+                    Id = 3,
+                    Courses = new Course[]
+                    {
+                        new Course{Id = 1, Name = "Math", Mark=50 },
+                        new Course{Id = 2, Name = "Science", Mark=50 },
+                        new Course{Id = 3, Name = "Literature", Mark=50 },
+                        new Course{Id = 4, Name = "Physichal Education", Mark=50 }
+                    }
+                },
+                new Student
                 {
-                    new Course{Id = 1, Name = "Math", Mark=40 },
-                    new Course{Id = 2, Name = "Science", Mark=40 },
-                    new Course{Id = 3, Name = "Literature", Mark=40 },
-                    new Course{Id = 4, Name = "Physichal Education", Mark=40 }
+                    Id = 4,
+                    Courses = new Course[]
+                    {
+                        new Course{Id = 1, Name = "Math", Mark=40 },
+                        new Course{Id = 2, Name = "Science", Mark=40 },
+                        new Course{Id = 3, Name = "Literature", Mark=40 },
+                        new Course{Id = 4, Name = "Physichal Education", Mark=40 }
+                    }
                 }
+ 
+            };
+
+            var graduated = new List<Tuple<bool, int, Standing>>();
+
+            foreach (var student in students)
+            {
+                graduated.Add(tracker.HasGraduated(diploma, student));
             }
 
+            Assert.IsTrue(graduated[0].Item3 == Standing.SumaCumLaude);
+            Assert.IsTrue(graduated[1].Item3 == Standing.SumaCumLaude);
+            Assert.IsTrue(graduated[2].Item3 == Standing.Average);
+            Assert.IsTrue(graduated[3].Item3 == Standing.Remedial);
 
-            //tracker.HasGraduated()
-        };
-            
-            var graduated = new List<Tuple<bool, STANDING>>();
-
-            foreach(var student in students)
-            {
-                graduated.Add(tracker.HasGraduated(diploma, student));      
-            }
-
-            
-            Assert.IsFalse(graduated.Any());
-
+            Assert.IsTrue(graduated[0].Item2 > 0);
+            Assert.IsTrue(graduated[1].Item2 > 0);
+            Assert.IsFalse(graduated[2].Item2 > 0);
+            Assert.IsFalse(graduated[3].Item2 > 0);
         }
-
-
     }
 }
